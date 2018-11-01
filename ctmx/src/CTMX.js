a4be7baf374087fa24f927c130ffde33b20712c7
César Chávez <design@pinata.tech>
2018-10-31 22:42:22 -0600

    CTMX - Route Fixing
    Fixes on files for correct importing with assets

--- ctmx/src/CTMX.js
+++ ctmx/src/CTMX.js
@@ -1,16 +1,18 @@
 import React, {Component} from 'react';
+import ctmxBrand from './assets/images/brand/brand--black.png';
 
 class CTMX extends Component {
     render () {
         return(
-            <section>
-                <h1>Heading 1</h1>
+            <section className="loader">
+                {/* <h1>Heading 1</h1>
                 <h2>Heading 2</h2>
                 <h3>Heading 3</h3>
                 <p className="t-body">Lorem Ipsum Dolor</p>
                 <p className="t-button">Button / Input</p>
                 <p className="t-caption">Button / Small</p>
-                <p className="t-footnote">Button / Small</p>
+                <p className="t-footnote">Button / Small</p> */}
+                <img src={ctmxBrand} alt="Circuito MX" />
             </section>
         )
     }
