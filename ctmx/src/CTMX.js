f262763f5bf3d7718be5af54139a03663d083501
César Chávez <design@pinata.tech>
2018-10-31 22:25:08 -0600

    CTMX-Start
    Adding principal files to start developing the react website for CTMX

---
+++ ctmx/src/CTMX.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+class CTMX extends Component {
+    render () {
+        return(
+            <section>
+                <h1>Heading 1</h1>
+                <h2>Heading 2</h2>
+                <h3>Heading 3</h3>
+                <p className="t-body">Lorem Ipsum Dolor</p>
+                <p className="t-button">Button / Input</p>
+                <p className="t-caption">Button / Small</p>
+                <p className="t-footnote">Button / Small</p>
+            </section>
+        )
+    }
+}
+
+export default CTMX;
